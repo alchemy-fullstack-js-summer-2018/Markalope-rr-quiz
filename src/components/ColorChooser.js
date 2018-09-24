9f7d439a83072f8d9de58d4d482eae6371b300d1
Mark Myers <markalope@gmail.com>
2018-09-23 19:36:08 -0700

    Trying to get the connect fucntion right

--- src/components/ColorChooser.js
+++ src/components/ColorChooser.js
@@ -1,19 +1,42 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Color from './Color';
+import AddColor from './AddColor';
+import { getColors, getSelectedColor } from './reducers';
+import { loadColors, addColor, selectColor } from './actions';
 
 class ColorChooser extends Component {
+
+  static propTypes = {
+    colors: PropTypes.array,
+    selectedColor: PropTypes.string,
+    addColor: PropTypes.func,
+    loadColors: PropTypes.func,
+    selectColor: PropTypes.func,
+    onSelect: PropTypes.func
+  }
   
   render() { 
     const { colors, addColor } = this.props;
     return ( 
-<Fragment>
+      <Fragment>
+        <h1>This is Color Chooser</h1>
 
+        <ul>
+          {colors.map(color => {
+            return <Color key={color} color={color} selectColor={selectColor} onSelect={this.handleSelect} />;
+          })}
+        </ul>
 
-</Fragment>
+        <AddColor addColor={addColor}/>
+
+      </Fragment>
 
     );
   }
 }
  
-export default ColorChooser;
+export default connect
+
+ColorChooser;
