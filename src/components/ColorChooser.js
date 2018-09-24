2f343012b53189794aa2d0fe90657b9b1ac9aabf
Mark Myers <markalope@gmail.com>
2018-09-23 19:27:34 -0700

    scaffolding for Color CHooser

---
+++ src/components/ColorChooser.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class ColorChooser extends Component {
+  state = {  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default ColorChooser;
