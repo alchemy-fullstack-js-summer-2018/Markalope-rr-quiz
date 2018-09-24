f3ca80ed8f92ef483e89690780f69c25808d8122
Mark Myers <markalope@gmail.com>
2018-09-23 19:08:48 -0700

    RainbowLightBulb.js written

--- src/components/RainbowLightBulb.js
+++ src/components/RainbowLightBulb.js
@@ -4,10 +4,44 @@ import connect from 'react-redux';
 import { getSelectedColor } from './reducers';
 
 class RainbowLightBulb extends Component {
-  state = {  }
-  render() { 
-    return (  );
+  state = {  
+    on: true
   }
+
+static propTypes = {
+  selectedColor: PropTypes.string
+}
+
+handleClick = () => {
+  this.setState(({ on }) => ({ on: false }));
+};
+
+
+render() { 
+
+  const { on } = this.state;
+  const { selectedColor } = this.props;
+
+
+  return ( 
+
+    <section>
+      <h1>This is RainbowLighBulb</h1>
+      <button onClick={this.handleClick}>
+        {on ? 'ON' : 'OFF'}
+      </button>
+
+      {on && 
+      <span style={{ color: selectedColor }}> <i className="far fa-lightbulb"></i></span>}
+    </section>
+
+
+  );
+}
 }
  
-export default RainbowLightBulb;
+export default connect(
+  state => ({
+    selectedColor: getSelectedColor(state)
+  })
+)(RainbowLightBulb);
