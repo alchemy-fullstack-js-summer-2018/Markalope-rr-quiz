24b7fd7cb2da612a304576ab8a2607f03946ebbd
Mark Myers <markalope@gmail.com>
2018-09-23 18:06:44 -0700

    starting reducers added to store

---
+++ src/components/reducers.js
@@ -0,0 +1,6 @@
+export const COLORS_LOAD = 'COLORS_LOAD';
+export const COLORS_ADD = 'COLORS_ADD';
+export const SELECT_COLOR = 'SELECT_COLOR';
+
+export const getColors = state => state.colors;
+export const getSelectedColor = state => state.selectedColor;
