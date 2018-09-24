9009f3925500dad7a129d6948d64925cfe50d0d4
Mark Myers <markalope@gmail.com>
2018-09-23 18:57:14 -0700

    acaffolding up RainbowLightBulb.js

---
+++ src/components/RainbowLightBulb.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import connect from 'react-redux';
+import { getSelectedColor } from './reducers';
+
+class RainbowLightBulb extends Component {
+  state = {  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default RainbowLightBulb;
