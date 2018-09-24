f2bfffb49dd94b459c8f9e7a25b50a30c43d26fa
Mark Myers <markalope@gmail.com>
2018-09-23 19:29:09 -0700

    added Fragment

--- src/components/ColorChooser.js
+++ src/components/ColorChooser.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class ColorChooser extends Component {
-  state = {  }
+  
   render() { 
-    return (  );
+    const { colors, addColor } = this.props;
+    return ( 
+<Fragment>
+
+
+</Fragment>
+
+    );
   }
 }
  
