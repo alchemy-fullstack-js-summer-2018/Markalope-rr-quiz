2907ad6c9c0da8ec3eca51cba60cab0feb8e1a50
Mark Myers <markalope@gmail.com>
2018-09-23 19:14:01 -0700

    OK doing Color.js now

---
+++ src/components/Color.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Color extends Component {
+  state = {  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default Color;
