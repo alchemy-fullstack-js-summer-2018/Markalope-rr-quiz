3f71fe65d9c264b669889e367fa22ef19d1e3e78
Mark Myers <markalope@gmail.com>
2018-09-23 19:19:42 -0700

    color.js initial code

--- src/components/Color.js
+++ src/components/Color.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Color extends Component {
-  state = {  }
-  render() { 
-    return (  );
-  }
+  
+  static propTypes = {
+    selectColor: PropTypes.func,
+    colorList: PropTypes.string
+  };
+
+handleClick = () => {
+  const { colorList, selectColor } = this.props;
+  selectColor(colorList);
+};
+
+
+
+render() { 
+
+  const { colorList } = this.props;
+
+  return ( 
+    <li>
+      <span onCLick={this.handleClick}>{colorList}</span>
+    </li>
+  );
+}
 }
  
 export default Color;
